d8b4870ab230a1fc9ac06407aa153d9b12442882
moulin wilfried <moulin.wilfried@gmail.com>
2020-01-20 22:22:26 +0100

    PROJ : clean

--- src/hoc/register-field.tsx
+++ src/hoc/register-field.tsx
@@ -1,7 +1,7 @@
 import { FormContext, FieldOptions } from "src/contexts/form-context";
 import { useContext, useEffect, useRef } from 'react';
 import * as React from 'react';
-import { useFormField } from 'src/hooks/use-form-field.js';
+import { useFormField } from 'src/hooks/use-form-field';
 import { Class } from 'hornet-js-utils/src/typescript-utils';
 
 export const RegisterField: Class<React.Component<any>> = (WrappedComponent: React.ComponentType<any>, props: any, fieldOptions?: FieldOptions<any>) => {
