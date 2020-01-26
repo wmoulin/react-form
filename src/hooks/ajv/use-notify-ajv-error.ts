633d71960c162b69e7e21cac596fe4903fdf30ed
moulin wilfried <moulin.wilfried@gmail.com>
2020-01-26 21:22:41 +0100

    PROJ : clean unused

--- src/hooks/ajv/use-notify-ajv-error.ts
+++ src/hooks/ajv/use-notify-ajv-error.ts
@@ -20,14 +20,16 @@ export default function useNotifyAjvError(notifId, id, formMessages, formContext
      * Déclenche les notifications correspondant aux éventuelles erreurs de validation
      * @param errors erreurs de validation de formulaire, éventuellement vides
      */
-    formContext.current.notifyErrors= (errors: Array<ErrorObject>): void => {
+    formContext.current.notifyErrors = (errors: Array<ErrorObject>): void => {
         if (errors) {
             const fieldsMessages = formMessages && formMessages.fields;
             const genericValidationMessages = I18nUtils.getI18n("form.validation", undefined, i18nMessages);
             const fields: { [key: string]: Element[] } = formContext.current.extractFields();
 
             const notificationsError: Notifications = getErrors(errors, fields, fieldsMessages, genericValidationMessages);
-
+            const dispatchEvent = (elt, errors, name) => {
+                elt.dispatchEvent(new CustomEvent('errors', { 'detail': {errors, name, id: formContext.current.form.current.id, stopOnFirst: true} }));
+            };
             /* Post-traitement des notifications concernant les champs d'autocomplétion */
             // TODO processAutocompleteErrors(fields, notificationsError);
 
@@ -46,9 +48,13 @@ export default function useNotifyAjvError(notifId, id, formMessages, formContext
                 if (errors && errors.length > 0) {
                     if(field.setState && typeof field.setState == "function") {
                         if ((field as any).setErrors && typeof (field as any).setErrors == "function") {
-                            (field as any).setErrors(errors)
+                            (field as any).setErrors(errors, () => {
+                                dispatchEvent(formContext.current.form.current, errors, (field.props as any).name);
+                            })
                         } else {
-                            field.setState({errors: errors});
+                            field.setState({errors: errors}, () => {
+                                dispatchEvent(formContext.current.form.current, errors, (field.props as any).name);
+                            });
                         }
                     } else {
                         field.parentElement && field.parentElement.classList.add("parent-field-error");
@@ -65,14 +71,18 @@ export default function useNotifyAjvError(notifId, id, formMessages, formContext
                             nextElt.classList.replace( "container-field-error-hidden", "container-field-error-show" );
                             nextElt.innerText =  errors.map((item) => item.text).join(",");
                         }
-                        field.dispatchEvent(new CustomEvent('errors', { 'detail': {errors, name: field.name} }));
+                        dispatchEvent(field, errors, field.name);
                     }
                 } else {
                     if(field.setState && typeof field.setState == "function") {
                         if ((field as any).setErrors && typeof (field as any).setErrors == "function") {
-                            (field as any).setErrors(undefined)
+                            (field as any).setErrors(undefined, () => {
+                                dispatchEvent(formContext.current.form.current, errors, (field.props as any).name);
+                            })
                         } else {
-                            field.setState({errors: undefined});
+                            field.setState({errors: undefined}, () => {
+                                dispatchEvent(formContext.current.form.current, errors, (field.props as any).name);
+                            });
                         }
                     } else {
                         field.parentElement && field.parentElement.classList.remove("parent-field-error");
@@ -84,8 +94,7 @@ export default function useNotifyAjvError(notifId, id, formMessages, formContext
                             nextElt.classList.replace( "container-field-error-show", "container-field-error-hidden" );
                             nextElt.innerText =  "";
                         }
-                        field.dispatchEvent(new CustomEvent('errors', { 'detail': {errors, name: field.name} }));
-
+                        dispatchEvent(field, errors, field.name);
                     }
                 }
             });
@@ -96,24 +105,24 @@ export default function useNotifyAjvError(notifId, id, formMessages, formContext
     };
 
     /**
-         * Supprime les nofifications d'erreurs et les erreurs associées à chaque champ de ce formulaire
-         */
-        formContext.current.cleanFormErrors = (): void => {
-            const fields: { [key: string]: Element[] } = formContext.current.extractFields();
-            for (const fieldName in fields) {
-                const field: Element = fields[fieldName][0];
+     * Supprime les nofifications d'erreurs et les erreurs associées à chaque champ de ce formulaire
+     */
+    formContext.current.cleanFormErrors = (): void => {
+        const fields: { [key: string]: Element[] } = formContext.current.extractFields();
+        for (const fieldName in fields) {
+            const field: Element = fields[fieldName][0];
 
-                // basic html
-                field.classList.remove("field-error");
-                field.parentElement && field.parentElement.classList.remove("parent-field-error");
-                let nextElt: HTMLElement = field.nextSibling as HTMLElement;
-                if(nextElt && nextElt.classList.contains("container-field-error")) {
-                    nextElt.classList.replace( "container-field-error-show", "container-field-error-hidden" );
-                    nextElt.innerText =  "";
-                }
+            // basic html
+            field.classList.remove("field-error");
+            field.parentElement && field.parentElement.classList.remove("parent-field-error");
+            let nextElt: HTMLElement = field.nextSibling as HTMLElement;
+            if(nextElt && nextElt.classList.contains("container-field-error")) {
+                nextElt.classList.replace( "container-field-error-show", "container-field-error-hidden" );
+                nextElt.innerText =  "";
             }
-            fireEvent(CLEAN_NOTIFICATION_EVENT.withData({ notifyId: notifId, idComponent: id }));
-        };
+        }
+        fireEvent(CLEAN_NOTIFICATION_EVENT.withData({ notifyId: notifId, idComponent: id }));
+    };
 
 
 
@@ -145,95 +154,95 @@ export default function useNotifyAjvError(notifId, id, formMessages, formContext
         return fieldName;
     }
 
-      /**
-   * Traite les erreurs de validation de formulaire : renvoie des notifications d'erreur.
-   * @param errors liste d'erreurs éventuellement vide
-   * @param fields Liste des champs du formulaire
-   * @param fieldsMessages messages spécifiques aux champs du formulaire
-   * @param genericValidationMessages messages d'erreur génériques
-   * @return {Notifications} les notifications correspondant aux erreurs de validation
-   */
-  function getErrors(errors: Array<ErrorObject>, fields: { [ key: string ]: Element[] }, fieldsMessages?: any, genericValidationMessages?: any): Notifications {
-    const notificationsError: Notifications = new Notifications();
+    /**
+     * Traite les erreurs de validation de formulaire : renvoie des notifications d'erreur.
+     * @param errors liste d'erreurs éventuellement vide
+     * @param fields Liste des champs du formulaire
+     * @param fieldsMessages messages spécifiques aux champs du formulaire
+     * @param genericValidationMessages messages d'erreur génériques
+     * @return {Notifications} les notifications correspondant aux erreurs de validation
+     */
+    function getErrors(errors: Array<ErrorObject>, fields: { [ key: string ]: Element[] }, fieldsMessages?: any, genericValidationMessages?: any): Notifications {
+        const notificationsError: Notifications = new Notifications();
 
-    for (let index: number = 0; index < errors.length; index++) {
-        const error = errors[ index ];
-        const erreurNotification = new NotificationType();
-        erreurNotification.id = "ACTION_ERREUR_" + index;
-        erreurNotification.text = error.message;
-        let fieldName: string = extractFieldName(error);
+        for (let index: number = 0; index < errors.length; index++) {
+            const error = errors[ index ];
+            const erreurNotification = new NotificationType();
+            erreurNotification.id = "ACTION_ERREUR_" + index;
+            erreurNotification.text = error.message;
+            let fieldName: string = extractFieldName(error);
 
-        let message: string = null;
-        if (fieldName) {
+            let message: string = null;
+            if (fieldName) {
 
-            erreurNotification.anchor = fieldName + "_anchor";
-            erreurNotification.field = fieldName;
-            erreurNotification.additionalInfos = error.params;
-            (erreurNotification as any).element = fields[ fieldName ][0];
+                erreurNotification.anchor = fieldName + "_anchor";
+                erreurNotification.field = fieldName;
+                erreurNotification.additionalInfos = error.params;
+                (erreurNotification as any).element = fields[ fieldName ][0];
 
-            let complement: any = { ...error.params };
+                let complement: any = { ...error.params };
 
-            // Gestion des champs editables d'un tableau
-            if (fields[ fieldName ] && (fields[ fieldName ][0] as any).props && (fields[ fieldName ][0] as any).props.title) {
-                const data = fieldName.split(".");
-                if (!isNaN(data[ data.length - 2 ] as any)) {
-                    fieldName = data[ data.length - 1 ];
-                    complement = { complement: (parseInt(data[ data.length - 2 ], 10) + 1).toString() };
+                // Gestion des champs editables d'un tableau
+                if (fields[ fieldName ] && (fields[ fieldName ][0] as any).props && (fields[ fieldName ][0] as any).props.title) {
+                    const data = fieldName.split(".");
+                    if (!isNaN(data[ data.length - 2 ] as any)) {
+                        fieldName = data[ data.length - 1 ];
+                        complement = { complement: (parseInt(data[ data.length - 2 ], 10) + 1).toString() };
+                    }
+                }
+
+                message = extractMessage(error.keyword, fieldName, fieldsMessages, genericValidationMessages, complement, fields[ fieldName ][0]);
+                if (message) {
+                    /* Surcharge du message produit par ajv */
+                    erreurNotification.text = message;
                 }
             }
+            notificationsError.addNotification(erreurNotification);
+        }
+        return notificationsError;
+    }
 
-            message = extractMessage(error.keyword, fieldName, fieldsMessages, genericValidationMessages, complement, fields[ fieldName ][0]);
-            if (message) {
-                /* Surcharge du message produit par ajv */
-                erreurNotification.text = message;
+    /**
+     * Génère le message d'erreur correspondant au mot-clé et au champ indiqués
+     * @param keyword mot clé de validation json-schema
+     * @param fieldName nom du champ (peut être un "path" tel que "ville.pays.id")
+     * @param fieldsMessages messages spécifiques aux champs du formulaire
+     * @param genericValidationMessages messages d'erreur génériques
+     * @param complement
+     * @return le message ou undefined lorsqu'aucun n'est défini pour le mot-clé indiqué
+     */
+    function extractMessage(keyword: string, fieldName: string, fieldsMessages?: any, genericValidationMessages?: any, complement?: any, field?: Element): string {
+        let message: string;
+        const specificMessage: any = get(fieldsMessages, fieldName + "." + keyword);
+    
+        if (isString(specificMessage)) {
+    
+            message = specificMessage;
+            if (complement) {
+                complement[ "field" ] = fieldName;
+                const intlMsg = new IntlMessageFormat(specificMessage, i18nMessages.locale);
+                message = intlMsg.format(complement);
+            }
+    
+        } else if (genericValidationMessages) {
+            const genericMessage: any = genericValidationMessages[ keyword ] || genericValidationMessages[ "generic" ];
+            
+            if(field && field.id){
+                var labels:HTMLCollection = document.getElementsByTagName('LABEL');
+                for (var i = 0; i < labels.length; i++) {
+                    if ((labels[i] as HTMLLabelElement).htmlFor != '' && (labels[i] as HTMLLabelElement).htmlFor == field.id) {
+                        fieldName = (labels[i] as HTMLLabelElement).innerText;         
+                    }
+                }
+            }
+    
+            if (isString(genericMessage)) {
+                const intlMsg = new IntlMessageFormat(genericMessage, i18nMessages.locale);
+                message = intlMsg.format({ field: fieldName });
             }
         }
-        notificationsError.addNotification(erreurNotification);
+        return message;
     }
-    return notificationsError;
-}
-
-  /**
-  * Génère le message d'erreur correspondant au mot-clé et au champ indiqués
-  * @param keyword mot clé de validation json-schema
-  * @param fieldName nom du champ (peut être un "path" tel que "ville.pays.id")
-  * @param fieldsMessages messages spécifiques aux champs du formulaire
-  * @param genericValidationMessages messages d'erreur génériques
-  * @param complement
-  * @return le message ou undefined lorsqu'aucun n'est défini pour le mot-clé indiqué
-  */
- function extractMessage(keyword: string, fieldName: string, fieldsMessages?: any, genericValidationMessages?: any, complement?: any, field?: Element): string {
-    let message: string;
-    const specificMessage: any = get(fieldsMessages, fieldName + "." + keyword);
-  
-    if (isString(specificMessage)) {
-  
-         message = specificMessage;
-         if (complement) {
-             complement[ "field" ] = fieldName;
-             const intlMsg = new IntlMessageFormat(specificMessage, i18nMessages.locale);
-             message = intlMsg.format(complement);
-         }
-  
-    } else if (genericValidationMessages) {
-         const genericMessage: any = genericValidationMessages[ keyword ] || genericValidationMessages[ "generic" ];
-        
-         if(field && field.id){
-             var labels:HTMLCollection = document.getElementsByTagName('LABEL');
-             for (var i = 0; i < labels.length; i++) {
-                 if ((labels[i] as HTMLLabelElement).htmlFor != '' && (labels[i] as HTMLLabelElement).htmlFor == field.id) {
-                     fieldName = (labels[i] as HTMLLabelElement).innerText;         
-                 }
-             }
-         }
-  
-         if (isString(genericMessage)) {
-             const intlMsg = new IntlMessageFormat(genericMessage, i18nMessages.locale);
-             message = intlMsg.format({ field: fieldName });
-         }
-    }
-    return message;
-  }
   
 
 }
